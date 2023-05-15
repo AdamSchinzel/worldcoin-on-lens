82f05507274039f4f08a0cd8aa799d1af3cde1c4
AdamSchinzel <schinzel.adam@gmail.com>
2023-05-16 01:18:39 +0200

    fix: type fail

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,12 +1,14 @@
 const copyToClipboard = async (text: string | undefined): Promise<void> => {
   if (navigator.clipboard) {
     try {
+      // @ts-ignore
       await navigator.clipboard.writeText(text);
     } catch (err) {
       console.error("Failed to copy text: ", err);
     }
   } else {
     const textarea = document.createElement("textarea");
+    // @ts-ignore
     textarea.value = text;
     document.body.appendChild(textarea);
     textarea.select();
